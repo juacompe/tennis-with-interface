61ca6adfe8d2738eec580d13f7d1d0715ea849e7
juacompe <juacompe@gmail.com>
2016-01-18 16:13:30 +0900

    game starts

---
+++ features/game_start.feature
@@ -0,0 +1,4 @@
+Feature: Game starts
+    Scenario: Game starting
+        Given Game starts
+        Then Score should be "Love All"
---
+++ features/step_definitions/game_start_steps.rb
@@ -0,0 +1,7 @@
+Given(/^Game starts$/) do
+    visit "http://localhost:3000/"
+end
+
+Then(/^Score should be "(.*?)"$/) do |arg1|
+      pending # express the regexp above with the code you wish you had
+end
