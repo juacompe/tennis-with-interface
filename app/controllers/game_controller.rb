a20e114d9c8b6fd4764c464aed5e7a4dae5131f1
juacompe <juacompe@gmail.com>
2016-01-18 16:59:07 +0900

    move score into controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,5 @@
 class GameController < ApplicationController
     def home
+        @score = "Love All"
     end
 end
--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe GameController, type: :controller do
             get :home
             expect(response).to render_template(:home)
         end
+        it "displays the current score" do
+            get :home
+            expect(assigns(:score)).to eq "Love All"
+        end
     end
 end
